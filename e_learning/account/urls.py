726a0fb1ea930c1579fd99dfa6f7fa9d5d3d6436
Ali <rouhi00ali@gmail.com>
2024-10-19 13:32:49 +0330

    urls for password reset is created

--- e_learning/account/urls.py
+++ e_learning/account/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('password-change/', auth_views.PasswordChangeView.as_view(),name='password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+    # reset password urls
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'), # step-1
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'), #step-2
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'), #step-3
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'), #step-4
+    
 ]
