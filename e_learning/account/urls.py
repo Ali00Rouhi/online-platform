47adbeaa1a6b7bb2b38b2e32006408244731c55c
Ali <rouhi00ali@gmail.com>
2024-10-20 16:25:47 +0330

    Profile system is created

--- e_learning/account/urls.py
+++ e_learning/account/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # path('login/', views.user_login, name='login')
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('password-change/', auth_views.PasswordChangeView.as_view(),name='password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
     # reset password urls
@@ -14,5 +14,6 @@ urlpatterns = [
     path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'), #step-3
     path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'), #step-4
     path('register/', views.register, name='register'),
-
+     path('edit/', views.edit, name='edit'),
+     
 ]
