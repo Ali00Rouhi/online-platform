fa2d3245440ff3ec54aa3c924632d7308e3f7d3b
Ali <rouhi00ali@gmail.com>
2024-10-21 12:01:07 +0330

    create and update view for content

--- e_learning/e_learning/urls.py
+++ e_learning/e_learning/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('course/', include()),
+    path('course/', include('courses.urls')),
     path('accounts/', include('account.urls'))
 ]
 
